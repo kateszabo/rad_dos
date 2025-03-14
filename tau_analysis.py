2b6759c49a9da4466b9710f99fda01d5d089e73d
sankeylab <sankeylab@gmail.com>
2025-03-14 17:15:26 -0400

    Update signal averaging and reorganize

--- tau_analysis.py
+++ tau_analysis.py
@@ -20,7 +20,7 @@ def trigger(x, y, threshold_y, refresh_x):
 
     return trig_i, trig_x, trig_y
 
-def average_pulses(signal_df, channel_key, threshold_y, refresh_x, window, lead):
+def average_pulses(signal_df, threshold_y, refresh_x, window, lead):
     trig_i, trig_x, trig_y = trigger(signal_df['time'], -signal_df['sync_trig'], threshold_y, refresh_x)
     n_pulses = len(trig_i)
 
@@ -30,14 +30,56 @@ def average_pulses(signal_df, channel_key, threshold_y, refresh_x, window, lead)
     pulses_time = np.zeros((n_pulses, window))
     pulses_trig = np.zeros((n_pulses, window))
     pulses_sig = np.zeros((n_pulses, window))
+    pulses_linout = np.zeros((n_pulses, window))
 
     for pulse_n, pulse_index in enumerate(trig_i):
         pulses_time[pulse_n] = signal_df['time'][pulse_index - start:pulse_index + end] - signal_df['time'][pulse_index] # time relative to pulse trigger
         pulses_trig[pulse_n] = signal_df['sync_trig'][pulse_index - start:pulse_index + end]
-        pulses_sig[pulse_n] = signal_df[channel_key][pulse_index - start:pulse_index + end]
+        pulses_sig[pulse_n] = signal_df['sig_ac'][pulse_index - start:pulse_index + end]
+        pulses_linout[pulse_n] = signal_df['lin_out'][pulse_index - start:pulse_index + end]
 
     mean_time = np.mean(pulses_time, axis=0)
     mean_signal = np.mean(pulses_sig, axis=0)
+    mean_linout = np.mean(pulses_linout, axis=0)
     mean_trigger = np.mean(pulses_trig, axis=0)
 
-    return pulses_time, mean_time, pulses_trig, mean_trigger, pulses_sig, mean_signal
+    return pulses_time, mean_time, pulses_trig, mean_trigger, pulses_sig, mean_signal, pulses_linout, mean_linout
+
+
+if __name__ == "__main__":
+    """ Example of how to use the functions """
+    import matplotlib.pyplot as plt
+
+    # import the csv
+    filename1 = '20250312_04_10MVfff.csv'
+    sig1 = pd.read_csv(filename1, header=0, names=['time', 'sig_ac', 'lin_out', 'sync_trig'], skiprows=[1,2], dtype=np.float64)
+
+    # remove unwanted channels and convert all units to V
+    sig1['sig_ac'] = sig1['sig_ac'] / 1000  # mV to V
+    sig1['lin_out'] = sig1['lin_out'] / 1000  # mV to V
+    sig1['time'] = sig1['time'] * 1000  # ms to us
+
+    pulses_time, mean_time, pulses_trig, mean_trigger, pulses_sig, mean_signal, pulses_linout, mean_linout \
+        = average_pulses(sig1, threshold_y=0.1, refresh_x=2000, window=5000, lead=3)
+    n_pulses=pulses_time.shape[0]
+
+    # Plot
+    fig, ax = plt.subplots(nrows=3, ncols=1, sharex=True, gridspec_kw={'height_ratios': [2, 2, 1]})
+
+    ax[0].plot(pulses_time.T, (pulses_sig.T), 'b', alpha=0.1)
+    ax[0].plot(mean_time, mean_signal, 'k')
+    ax[0].set_title(f'Averaged AC signal ({n_pulses} pulses)')
+
+    ax[1].plot(pulses_time.T, (pulses_linout.T), 'purple', alpha=0.1)
+    ax[1].plot(mean_time, mean_linout, 'k')
+    ax[1].set_title(f'Linear output ({n_pulses} pulses)')
+
+    ax[2].plot(pulses_time.T, pulses_trig.T, 'r', alpha=0.1)
+    ax[2].plot(mean_time, mean_trigger, 'k')
+    ax[2].set_title(f'Sync trigger ({n_pulses} pulses)')
+    ax[2].set_xlabel('Time (µs)')
+
+    plt.setp(ax, xlim=(-100, 100))
+
+    fig.supylabel('Signal (V)')
+    fig.suptitle(filename1)
